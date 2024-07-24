fixed fid computation

src/models/unet.py
@@ -117,7 +117,7 @@ class UNet(nn.Module):
 
         if self.use_context:
             context = nn.Dense(self.pos_emb_dim)(context)
-            context = cast_dim(context, x.shape)
+            context = cast_dim(context, x.ndim)
         else:
             context = None
 
@@ -130,7 +130,7 @@ class UNet(nn.Module):
                 is_attention = isinstance(layer_block, AttentionBlock)
                 if is_attention:
                     x = layer_block(
-                        x, pos_emb, deterministic=not train, context=context)
+                        x, context, pos_emb, deterministic=not train)
                 else:
                     x = layer_block(x, pos_emb, deterministic=not train)
 
@@ -140,7 +140,7 @@ class UNet(nn.Module):
             is_attention = isinstance(layer, AttentionBlock)
             if is_attention:
                 x = layer(
-                    x, pos_emb, deterministic=not train, context=context)
+                    x, context, pos_emb, deterministic=not train)
             else:
                 x = layer(x, pos_emb, deterministic=not train)
 
@@ -150,7 +150,7 @@ class UNet(nn.Module):
                 is_attention = isinstance(layer_block, AttentionBlock)
                 if is_attention:
                     x = layer_block(
-                        x, pos_emb, deterministic=not train, context=context)
+                        x, context, pos_emb, deterministic=not train)
                 else:
                     x = layer_block(x, pos_emb, deterministic=not train)
 

src/training/fid.py
@@ -15,7 +15,7 @@ class FID:
     Based on https://github.com/matthias-wright/jax-fid/blob/main/jax_fid/fid.py
     """
 
-    def __init__(self, img_size: tuple, reference_dir: str, batch_size: int = 128, scale_to: int = 256):
+    def __init__(self, img_size: tuple, reference_dir: str, batch_size: int = 512, scale_to: int = 256):
         self.batch_size = batch_size
         self.img_size = img_size
         self.reference_dir = reference_dir
@@ -31,7 +31,7 @@ class FID:
         self.apply_fn = jax.jit(partial(model.apply, train=False))
 
     def precompute(self, save_dir=None):
-        mu, sigma = jf.compute_statistics(
+        mu, sigma = self.compute_statistics(
             self.reference_dir, self.params, self.apply_fn, self.batch_size, self.img_size)
         self.ref_mu, self.ref_sigma = mu, sigma
 
@@ -46,8 +46,7 @@ class FID:
 
         mu2, sigma2 = self.compute_statistics(
             synthetic_dir, self.params, self.apply_fn, self.batch_size, self.img_size)
-        print(
-            f"Ref - mu: {self.ref_mu}, sigma: {self.ref_sigma}; Synth - mu: {mu2}, sigma: {sigma2}")
+
         fid_score = jf.compute_frechet_distance(
             self.ref_mu, mu2, self.ref_sigma, sigma2, eps=1e-3)
 
@@ -61,17 +60,24 @@ class FID:
 
         images = []
         for f in tqdm(os.listdir(path)):
-            img = Image.open(os.path.join(path, f))
+            try:
+                img = Image.open(os.path.join(path, f))
+                if img.mode == "L":
+                    img = img.convert("RGB")
+                img = img.resize(
+                    size=(self.scale_to, self.scale_to),
+                    resample=Image.BILINEAR,
+                )
+                img = np.array(img) / 255.0
+                if img.shape == (self.scale_to, self.scale_to, 3):
+                    images.append(img)
+                else:
+                    print(f"Skipping file {f}: Unexpected shape {img.shape}")
+            except Exception as e:
+                print(f"Skipping file {f}: {e}")
 
-            if img.mode == "L":
-                img = img.convert("RGB")
-
-            img = img.resize(
-                size=(self.scale_to, self.scale_to),
-                resample=Image.BILINEAR,
-            )
-            img = np.array(img) / 255.0
-            images.append(img)
+        if not images:
+            raise ValueError("No valid images found in the directory.")
 
         num_batches = int(len(images) // batch_size)
         act = []
@@ -83,6 +89,14 @@ class FID:
             act.append(pred.squeeze(axis=1).squeeze(axis=1))
         act = jnp.concatenate(act, axis=0)
 
+        if act.shape[0] == 0:
+            raise ValueError(
+                "No activations computed; check image preprocessing.")
+
         mu = np.mean(act, axis=0)
         sigma = np.cov(act, rowvar=False)
+        if np.any(np.isnan(mu)) or np.any(np.isnan(sigma)):
+            print(
+                f"NaN detected in computed statistics. {mu: {mu}}; sigma: {sigma}")
+
         return mu, sigma

src/training/trainer.py
@@ -45,7 +45,6 @@ def train_step(random_key: Any,
     def loss_fn(params):
         t1_noise_dim = cast_dim(t1, noise.ndim)
         t2_noise_dim = cast_dim(t2, noise.ndim)
-        context = cast_dim(context, noise.ndim)
 
         xt1_raw = x + t1_noise_dim * noise
         xt2_raw = x + t2_noise_dim * noise
@@ -94,7 +93,8 @@ class ConsistencyTrainer:
         self.device_batch_shape = (device_batch_size, *img_shape)
 
         init_input = jnp.ones(self.device_batch_shape)
-        init_context = jnp.ones(self.config["context_dim"])
+        init_context = jnp.ones(
+            (device_batch_size, *self.config["context_dim"]))
         init_time = jnp.ones((device_batch_size,))
         model_params = model.init(
             init_key, init_input, init_context, init_time, train=True)
@@ -109,6 +109,10 @@ class ConsistencyTrainer:
 
     def train(self, train_steps: int):
         parallel_state = replicate(self.state)
+
+        if run_eval:
+            fid_score = self.run_eval()
+            wandb.log({"fid_score": fid_score}, step=step)
 
         with trange(self.checkpoint_step, train_steps, initial=self.checkpoint_step, total=train_steps) as steps:
             cumulative_loss = 0.0
@@ -193,8 +197,8 @@ class ConsistencyTrainer:
                 self._save(
                     parallel_state, step, save_checkpoint, save_snapshot)
 
-                run_eval = self.config["run_evals"] and (step == 0 or (
-                    step + 1) % self.config["eval_frequency"] == 0)
+                run_eval = self.config["run_evals"] and (
+                    step + 1) % self.config["eval_frequency"] == 0
                 if run_eval:
                     self.state = unreplicate(parallel_state)
                     fid_score = self.run_eval()
@@ -214,7 +218,7 @@ class ConsistencyTrainer:
             self.save_snapshot(step)
 
     def save_snapshot(self, step):
-        outputs = self.generate(self.snapshot_key, classes=None)
+        outputs = self.generate(self.snapshot_key, context=None)
 
         pillow_outputs = [reverse_transform(output) for output in outputs]
 
@@ -263,7 +267,7 @@ class ConsistencyTrainer:
                                   self.consistency_config["sigma_data"],
                                   self.consistency_config["sigma_min"],
                                   self.consistency_config["sigma_max"],
-                                  classes=context)
+                                  context=context)
 
     def generate_cfg(self, key, context):
         cond_key, uncond_key = random.split(key)

src/utils.py
@@ -2,9 +2,12 @@ import jax
 import jax.numpy as jnp
 
 
-def cast_dim(x, n_dims):
+def cast_dim(x, n_dims, start_dim=None):
     dim_diff = n_dims - x.ndim
-    extra_dim_indices = [-i for i in range(1, dim_diff+1)]
+    if start_dim is None:
+        extra_dim_indices = [-i for i in range(1, dim_diff+1)]
+    else:
+        extra_dim_indices = [start_dim + i for i in range(dim_diff)]
     return jnp.expand_dims(x, extra_dim_indices)
 
 